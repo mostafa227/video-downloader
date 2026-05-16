4193fb4ec7aaef941d88a05e5df7056bfd7ce93d
fool <161215156+mostafa227@users.noreply.github.com>
2026-05-16 23:08:08 +0300

    app.py

--- app.py
+++ app.py
@@ -479,5 +479,6 @@ def download_file(job_id):
 
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
